0d2dba380c10b98b8f6b9b2e62c422fd94e1d1e5
Yahya <74861627+RemoteSoul-hub@users.noreply.github.com>
2022-08-26 22:04:07 +0100

    Update Posts.js

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -34,7 +34,7 @@ const Posts = ({ getPosts, post: { posts } }) => {
     <Grow in>
       <Container>
         <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-          <Grid item xs={12} sm={6} md={9}>
+          <Grid item xs={12} sm={7}>
             {posts.map((post) => (    
             <PostItem key={post._id} post={post} />
             ))}
